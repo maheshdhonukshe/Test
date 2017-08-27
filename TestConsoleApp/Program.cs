deccc308a6bd916208b1dc9bdc66649e71236815
HQ\dhonukshem <MaheshD@OvationsGroup.com>
2017-08-27 20:05:41 +0200

    First commit

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TestConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program p = new Program();
+                
+                string csvPath = ConfigurationManager.AppSettings["CsvPath"];
+                string outputFolder = ConfigurationManager.AppSettings["OutputFolder"];
+
+                DataTable dt = p.ReadCsvIntoDataTable(csvPath);
+
+                List<string> list1 = p.GetList1(dt);
+                p.WriteToFile(list1, string.Concat(outputFolder, "List1.txt"));
+
+                List<string> list2 = p.GetList2(dt);
+                p.WriteToFile(list2, string.Concat(outputFolder, "List2.txt"));
+
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception : " + ex.Message);
+                Console.WriteLine("StackTrace : " + ex.StackTrace);
+            }
+        }
+
+        public DataTable ReadCsvIntoDataTable(string filePath)
+        {
+            DataTable dtCsv = new DataTable();
+            string fulltext;
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    fulltext = sr.ReadToEnd().ToString(); //read full file text  
+                    string[] rows = fulltext.Split('\n'); //split full file text into rows  
+                    for (int i = 0; i < rows.Count() - 1; i++)
+                    {
+                        string[] rowValues = rows[i].Split(','); //split each row with comma to get individual values  
+                        {
+                            if (i == 0)
+                            {
+                                for (int j = 0; j < rowValues.Count(); j++)
+                                {
+                                    dtCsv.Columns.Add(rowValues[j]); //add headers  
+                                }
+                            }
+                            else
+                            {
+                                DataRow dr = dtCsv.NewRow();
+                                for (int k = 0; k < rowValues.Count(); k++)
+                                {
+                                    dr[k] = rowValues[k].ToString();
+                                }
+                                dtCsv.Rows.Add(dr); //add other rows  
+                            }
+                        }
+                    }
+                }
+            }
+
+            return dtCsv;
+        }
+
+        public List<string> GetList1(DataTable dt)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>(); //to hold firstname/lastname and count of it
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                string firstName = dr["FirstName"].ToString();
+                if (dict.ContainsKey(firstName))
+                    dict[firstName] = dict[firstName] + 1;
+                else
+                    dict.Add(firstName, 1);
+
+                string lastName = dr["LastName"].ToString();
+                if (dict.ContainsKey(lastName))
+                    dict[lastName] = dict[lastName] + 1;
+                else
+                    dict.Add(lastName, 1);
+            }
+                        
+            //sort by count descending and name ascending
+            var sortedDict = dict.OrderByDescending(e => e.Value).ThenBy(e => e.Key);
+
+            //convert to list and return it
+            return sortedDict.Select(pair => string.Concat(pair.Key, ", ", pair.Value)).ToList();
+        }
+
+        public List<string> GetList2(DataTable dt)
+        {
+            //can't use dictionary here because multiple persons can have same address
+            //so create a list of instances of an anonymous class which has 2 properties, Street and Address
+            List<dynamic> tmpList = new List<object>(); 
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                string address = dr["Address"].ToString();
+                string[] addressParts = address.Split(' '); //to extract street name from address
+                tmpList.Add(new { Street = addressParts[1], Address = address });
+            }
+
+            tmpList.Sort((o1, o2) => o1.Street.CompareTo(o2.Street)); //sort list by street
+
+            List<string> list = tmpList.Select(o => (string)o.Address).ToList(); //return new list containing only addresses
+            return list;
+        }
+
+        public void WriteToFile(List<string> list, string filePath)
+        {
+            using (StreamWriter file = new StreamWriter(filePath))
+            {
+                foreach (string line in list)
+                {
+                    file.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConsoleApp;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        string filePath = "data.csv";
+
+        [TestMethod]
+        public void TestReadCsv()
+        {
+            Program p = new Program();
+            DataTable dt = p.ReadCsvIntoDataTable(filePath);
+            Assert.IsNotNull(dt);
+        }
+
+        [TestMethod]
+        public void TestGetList1()
+        {
+            List<string> expectedList = new List<string>();
+            expectedList.Add("Brown, 2");
+            expectedList.Add("Clive, 2");
+            expectedList.Add("Graham, 2");
+            expectedList.Add("Howe, 2");
+            expectedList.Add("James, 2");
+            expectedList.Add("Owen, 2");
+            expectedList.Add("Smith, 2");
+            expectedList.Add("Jimmy, 1");
+            expectedList.Add("John, 1");
+
+            Program p = new Program();
+            DataTable dt = p.ReadCsvIntoDataTable(filePath);
+            List<string> list = p.GetList1(dt);
+
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+
+        [TestMethod]
+        public void TestGetList2()
+        {
+            List<string> expectedList = new List<string>();
+            expectedList.Add("65 Ambling Way");
+            expectedList.Add("8 Crimson Rd");
+            expectedList.Add("12 Howard St");
+            expectedList.Add("102 Long Lane");
+            expectedList.Add("94 Roland St");
+            expectedList.Add("78 Short Lane");
+            expectedList.Add("82 Stewart St");
+            expectedList.Add("49 Sutherland St");
+
+            Program p = new Program();
+            DataTable dt = p.ReadCsvIntoDataTable(filePath);
+            List<string> list = p.GetList2(dt);
+
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+
+        [TestMethod]
+        public void TestWriteToFile()
+        {
+            List<string> list = new List<string>();
+            list.Add("line 1");
+            list.Add("line 2");
+
+            Program p = new Program();
+            p.WriteToFile(list, "Test.txt");
+
+            Assert.IsTrue(File.Exists("Test.txt"));
+        }        
+    }
+}
